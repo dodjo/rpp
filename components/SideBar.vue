aa11292be6156bf15a76b93414087392538dba78
Vladimir Chmyr <chvn.2uit@yandex.ru>
2022-09-17 15:22:52 +0300

    Настройка проекта

---
+++ components/SideBar.vue
@@ -0,0 +1,72 @@
+<template>
+  <aside class='sidebar'>
+    <div class='sidebar__header'>
+      <svg-icon class='logo' name='logo'/>
+    </div>
+    <div class='sidebar__body'>
+      <div class='menu'>
+        <nuxt-link to="/" class='menu__item'>
+          <svg-icon name='sidebar/icon-cup'/>
+          Тарифы
+        </nuxt-link>
+        <nuxt-link to="/help" class='menu__item'>
+          <svg-icon name='sidebar/icon-cup'/>
+          Тарифы
+        </nuxt-link>
+      </div>
+    </div>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: 'SideBar'
+}
+</script>
+
+<style lang='scss' scoped>
+.sidebar {
+  height: 100%;
+  border-radius: 0 20px 0 0;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+  background-color: $color5;
+  &__header {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100px;
+  }
+
+  .logo {
+    width: 98px;
+    height: 25px;
+  }
+
+  .menu {
+    &__item {
+      display: flex;
+      align-items: center;
+      line-height: 14px;
+      color: $color2;
+      text-decoration: none;
+      gap: 5px;
+      padding: 10px 23px 10px 15px;
+      border-bottom: 1px solid $sidebar-border-color;
+      transition: $transition;
+      cursor: pointer;
+      &:hover {
+        background-color: $sidebar-menu-hover;
+      }
+      .icon {
+        width: 15px;
+        height: 15px;
+      }
+    }
+
+    &__item.active {
+      color: $color5;
+      background-color: $color2;
+    }
+  }
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class='main'>
+    <div class='main__sidebar'>
+      <side-bar/>
+    </div>
+    <div class='main__page'>
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+import SideBar from '~/components/SideBar'
+export default {
+  name: 'default',
+  components: { SideBar }
+}
+</script>
+
+<style lang='scss' scoped>
+.main {
+  display: grid;
+  grid-template-columns: 250px 1fr;
+  height: 100vh;
+}
+</style>
